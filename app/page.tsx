c875f55da35cf6005da87b44da61bc2c5018f5cc
Atul Deep Singh <999.atulsingh@gmail.com>
2025-05-23 01:23:03 +0530

    added shad cn

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h2>Subscribe to X my boys</h2>
+      <Button variant={"destructive"}>click me</Button>
     </div>
   );
 }
